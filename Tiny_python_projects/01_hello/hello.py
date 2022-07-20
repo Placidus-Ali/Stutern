deecc87ef07021375965e3707503886dd255765a
Placidusali@gmail.com <placidusali@gmail.com>
2022-07-20 21:57:44 +0100

    added

--- Tiny_python_projects/01_hello/hello.py
+++ Tiny_python_projects/01_hello/hello.py
@@ -3,10 +3,15 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description= 'say Hello')
-parser.add_argument('name', help='Name to greet')
-args = parser.parse_args()
-name = args.name
+def get_args():
+    parser = argparse.ArgumentParser(description= 'say Hello')
+    parser.add_argument('-n', '--name', metavar="Name", default='World' , help="name to greet")
+    return parser.parse_args()
 
-print("Hello, "+ name + "!")
+def main():
+    args = get_args()
+    name = args.name
+    print("Hello, " + name + "!")
 
+if __name__ == '__main__':
+    main()
