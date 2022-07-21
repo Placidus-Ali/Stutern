76545d9864c4046849ff47bdcdec160037e42ee8
Placidusali@gmail.com <placidusali@gmail.com>
2022-07-21 09:09:57 +0100

    added

--- Tiny_python_projects/01_hello/hello.py
+++ Tiny_python_projects/01_hello/hello.py
@@ -1,3 +1,33 @@
-#!/user/bin/env python3
-# Say Hello World
-print("Hello, World!")
+#!/usr/bin/env python3
+
+import argparse
+from email import parser
+import os
+import sys
+
+#--------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description= 'Say Hello'
+        formatter_class= argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--n', 
+                        '--name',
+                        help='Name to greet',
+                        metavar='name',
+                        type=str
+                        default='World')
+    
+    return parser.parse_args()
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print('Hello, + args.name' + '!')
+
+if __name__ == '__main__' :
+    main()
+    
