c4908080e512e5c0f1c6dbf0834e83903ed12a05
Placidusali@gmail.com <placidusali@gmail.com>
2022-07-22 07:42:38 +0100

    added

--- Tiny_python_projects/01_hello/hello.py
+++ Tiny_python_projects/01_hello/hello.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 #say hello
-print('Hello World!')
+
+import argparse
+from email import parser
+
+parser = argparse.ArgumentParser(description= 'say hello')
+parser.add_argument('-n', '--name', metavar='Name', default='World', help='Name to greet')
+args = parser.parse_args()
+name = args.name
+
+print('Hello ' + name + '!')
