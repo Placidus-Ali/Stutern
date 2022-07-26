d50e281120930d55e59d15032ee28c38ceaaaec5
Placidusali@gmail.com <placidusali@gmail.com>
2022-07-27 00:51:45 +0100

    added new file

--- model3_lesson4/Bank.py
+++ model3_lesson4/Bank.py
@@ -5,26 +5,23 @@ class BankAccount:
         print('The account is created and initial account balance is 0')
 
     def deposit(self):
-        amount = float(input("User enters the amount to deposite: "))
+        amount = float(input("Now enter the amount you want to deposit: "))
         self.balance = self.balance + amount
-        print("You have successfully depositted %f" %self.balance)
+        print("You have successfully depositted %f" % self.balance)
 
     def withdraw(self):
-        amount = float(input("User enters the amount to be withdrawn: "))
+        amount = float(input("Now enter the amount you want to withdraw: "))
         if self.balance >= amount:
                 self.balance = self.balance - amount
                 print("Amount withdrawn successfully")
 
         else:
-            print("Insuficient balance. Account balance is %f" %self.balance)
+            print("Insuficient balance. Your current account balance is %f" % self.balance)
 
     def enquiry(self):
-        print("Current balance is %f" %self.balance)
+        print("Current balance is %f" % self.balance)
 
-
-
-AccessBank = BankAccount
+AccessBank = BankAccount()
 AccessBank.deposit()
 AccessBank.withdraw()
 AccessBank.enquiry()
-
