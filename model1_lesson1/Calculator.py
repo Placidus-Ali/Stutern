bb17e243d872cfb8228590cb17af01a35b420a4a
Placidusali@gmail.com <placidusali@gmail.com>
2022-08-12 13:28:57 +0100

    added new file

---
+++ model1_lesson1/Calculator.py
@@ -0,0 +1,16 @@
+# Exponential Calculator
+
+print("Exponential Calculator")
+x = int(input())
+y = int(input())
+
+# give a variable name in the calculation
+num = x**y
+
+if y == 0:
+    print("x =", 1)
+
+else:
+    print("x =", x)
+
+print(num)
